5954915a5f081970761447a73a349c9608b4dd59
wellsilver <wellsilver2016@gmail.com>
2024-04-07 18:16:12 -0400

    only one instance can run

--- src/graphics.hpp
+++ src/graphics.hpp
@@ -3,14 +3,19 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
 
-void grun(bool *active) {
-  sf::RenderWindow window(sf::VideoMode(640, 480, 32), "KChat");
-  sf::Event p;
+void grun(bool *active, bool *guiactive) {
   while (*active) {
-    window.pollEvent(p);
-    if (p.type == p.Closed) *active=0;
+    if (*guiactive) {
+      sf::RenderWindow window(sf::VideoMode(640, 480, 32), "KChat");
+      sf::Event p;
+      while (*guiactive) {
+        window.pollEvent(p);
+        if (p.type == p.Closed) *guiactive=0;
 
 
-    window.display();
+        window.display();
+      }
+      window.close();
+    }
   }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,25 @@
 #include "graphics.hpp"
 #include "network.hpp"
 
+// tell the other instance to reactivate its gui
+void notifyext() {
+  sf::TcpSocket t;
+  t.connect(sf::IpAddress::LocalHost, 64888);
+  t.send(sf::Packet() << "gui");
+  t.disconnect();
+}
+
 int main() {
   sf::UdpSocket server;
+  sf::TcpListener local_server;
+  if (local_server.listen(64888, sf::IpAddress::LocalHost) == sf::Socket::Status::Error) {notifyext();return -1;}
+  
   sf::Clock time;
 
   bool gui_active = true;
   bool active = true;
 
-  std::thread thrd_grphc(grun, &gui_active);
+  std::thread thrd_grphc(grun, &active, &gui_active);
   std::thread thrd_ntwrk(nrun, &active);
   // poll... forever....
   while (active) std::this_thread::sleep_for(std::chrono::seconds(1));
