e2f5956761b8f1ab568128fccf0f7398b5368c6e
wellsilver <wellsilver2016@gmail.com>
2024-04-13 21:40:00 -0400

    this does not work

--- src/graphics.hpp
+++ src/graphics.hpp
@@ -3,7 +3,23 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
 
+#include <thread>
+
+// thread to toggle guiactive when requested
+void ensurerun(bool *active, sf::TcpListener *local_server, bool *guiactive) {
+  sf::TcpSocket n;
+  std::string recv;
+  sf::Packet packet;
+  while (*active) {
+    local_server->accept(n);
+    n.receive(packet);
+    packet >> recv;
+    if (recv == "gui") *guiactive = true;
+  }
+}
+
 void grun(bool *active, sf::TcpListener *local_server, bool *guiactive) {
+  std::thread(ensurerun, active, local_server, guiactive);
   while (*active) {
     if (*guiactive) {
       sf::RenderWindow window(sf::VideoMode(640, 480, 32), "KChat");
