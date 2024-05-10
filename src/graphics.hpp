cc2c4fd27fa14ac1ee1fca743ae4aa9b65eefed8
wellsilver <wellsilver2016@gmail.com>
2024-05-09 21:46:30 -0400

    Revert "graphics.hpp will also need local_server"
    This reverts commit 54908a15647473fd5f6ab51469eb1f9d85612672.

--- src/graphics.hpp
+++ src/graphics.hpp
@@ -3,23 +3,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
 
-#include <thread>
-
-// thread to toggle guiactive when requested
-void ensurerun(bool *active, sf::TcpListener *local_server, bool *guiactive) {
-  sf::TcpSocket n;
-  std::string recv;
-  sf::Packet packet;
-  while (*active) {
-    local_server->accept(n);
-    n.receive(packet);
-    packet >> recv;
-    if (recv == "gui") *guiactive = true;
-  }
-}
-
-void grun(bool *active, sf::TcpListener *local_server, bool *guiactive) {
-  std::thread(ensurerun, active, local_server, guiactive);
+void grun(bool *active, bool *guiactive) {
   while (*active) {
     if (*guiactive) {
       sf::RenderWindow window(sf::VideoMode(640, 480, 32), "KChat");
