084b154f1d8a04b518c163a04e01cfd8d88a5485
wellsilver <wellsilver2016@gmail.com>
2024-10-13 17:10:34 -0400

    base

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <vector>
+#include <string>
 
 #include <ncurses.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <signal.h>
 
-int main() {
-  WINDOW *win = initscr();
+std::string composing;
+
+bool end = false; // if its time to exit
+unsigned int tasks = 0; // running tasks
+
+WINDOW *win;
+
+void *render(void *) {
+  tasks++;
+  win = initscr();
   noecho();
 
+  clear();
   refresh();
 
-  getch();
+  while (end == false) {
+    wchar_t c = getwchar();
+    if (c != BUTTON_SHIFT && c != 'q') {
 
+    } else if (c == 'q') end = true;
+    
+    refresh();
+  }
 
-  sleep(1);
   endwin();
+  tasks--;
+  return 0;
+}
+
+void intHandler(int dummy) {
+  end = true;
+}
+
+int main() {
+  pthread_t rd;
+  pthread_create(&rd, 0, render, 0);
+
+  signal(SIGINT, intHandler);
+  // wait indefinitely until ended
+  while (tasks != 0 || end == false) sleep(0);
+  return 0;
 }
